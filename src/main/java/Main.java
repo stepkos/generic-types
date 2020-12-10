7f29919ca4863aa090458016fc44f04df2480bc1
stepkos <52202262+stepkos@users.noreply.github.com>
2020-12-10 23:11:20 +0100

    Yess... I know I forget to add to .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+//        Skeleton s1 = new Skeleton(-1, 2);
+//        Zombie s2 = new Zombie(2, 6);
+//        System.out.println(s1.countDistance(s2));
+
+        Monster m1 = new Skeleton();
+        m1.specialAttack(10);
 
     }
 }
